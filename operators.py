4abdcb7f793ccd249c1f252cd6be4ca270455329
Edward Dume <edwarddume55@gmail.com>
2023-10-17 13:46:39 +0300

    adding operators

---
+++ operators.py
@@ -0,0 +1,28 @@
+# arithmetic operators
+x=7
+y=3
+# add
+print(x+y)
+# subtract
+print(x-y)
+# multiplication
+print(x*y)
+# divide
+print(x/y)
+# modulus
+print(x%y)
+
+# comparison operators
+a=23
+b=31
+print(f"{a} is not equal to {b} {a!=b}")
+print(f"{a} is less than  {b} {a>b}")
+print(f"{b} is less than or equals to {b} {a>=b}")
+print(f"{a} is not equal to {b} {a==b}")
+
+# logical operators
+c=5
+# both statement true
+print(f"is this statement True { c>5 and c<5}")
+# either statement true
+print(f"is this statement True { c>5 or c<=5}")
